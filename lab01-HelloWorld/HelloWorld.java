1680d36cc80f3d045c85bfa9efaea4a914c45580
Jose Sierra <jas216@lehigh.edu>
2014-08-29 13:34:25 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+// Jose Sierra
+// demo 0
+// hello world java program
+//
+// first compile the program
+//      javac helloworld.java
+//  run the program
+//      java helloworld//
+
+// define a class
+public class HelloWorld{
+    
+    // add main method
+    public static void main(String[] args) {
+        
+        // print the statement with box around
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("#                       #");
+        System.out.println("#   Hello World 2014!!  #");
+        System.out.println("#                       #");
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("test");
+        
+        //test/testtest
+        
+    }
+}
+
