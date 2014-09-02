41f35eb788a0353ac4dce0d903bf0603bd4b2568
Jose Sierra <jas216@lehigh.edu>
2014-09-02 19:13:29 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+// Jose Sierra
+// hw01
+// WelcomeClass program to display a "welcome message"
+//
+
+// define a class
+public class WelcomeClass{ 
+    
+    // main method
+    public static void main(String[] args) {
+        // print statement below
+         System.out.println("     -----------     ");
+         System.out.println("     | WELCOME |     ");
+         System.out.println("     -----------     ");
+         System.out.println("   ^  ^  ^  ^  ^  ^  ");
+         System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+         System.out.println(" |-J--A--S--2--1--6-|");
+         System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /   ");
+         System.out.println("   v  v  v  v  v  v  ");
+         System.out.println("My Brother and I had the best Ramen we have had so far,");
+         System.out.println("it was last Saturday, at a small restaurant in Philly.");
+        
+    }
+    
+}
+
