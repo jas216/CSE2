6cc54d2fcdfbbe6481cf2001f7a397fb549416dc
Jose Sierra <jas216@lehigh.edu>
2014-09-23 03:46:02 +0000

    Month

---
+++ hw04/CourseNumber.java
@@ -0,0 +1,9 @@
+// Jose Sierra
+// CSE 2
+// hw04 - Program #3
+/* The following program reads a 6 digit number, and prints out its corresponding 
+semester accordign to the lehigh university semester numbering system */
+
+
+import java.util.Scanner;   // imports a user input device
+
