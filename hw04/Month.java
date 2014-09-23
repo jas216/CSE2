4abec8482a1f2a2db5cae24d1d2dd174aff18fbb
Jose Sierra <jas216@lehigh.edu>
2014-09-23 03:30:59 +0000

    Month

--- hw04/Month.java
+++ hw04/Month.java
@@ -20,10 +20,10 @@ public class Month {
         int month, year;
         
         System.out.print("Enter an integer corresponding to a month of the year (1-12): ");
-        if (myScanner.hasNextInt()) {       // checks for input to be an integer
+        if (myScanner.hasNextInt()) {       // the first if statement checks the input to be an integer
             // define input as month
             month = myScanner.nextInt();
-            if (month <= 0){
+            if (month <= 0 || month > 12){
                 System.out.println("You did not enter a number between 1-12");
             }
             
@@ -31,12 +31,13 @@ public class Month {
                 System.out.print("Enter a giving year (integer): ");
                 if (myScanner.hasNextInt()) {
                     year = myScanner.nextInt();
+                
                     if (year < 0) {
-                        System.out.println("You did not enter a positve integer");
+                        System.out.println("You did not enter a positve integer");  // returns error if input was not an integer
                     }      
                     
                     else {
-                        boolean leap = (year % 4 == 0 && year % 100 != 0)||(year % 400 == 0);
+                        boolean leap = (year % 4 == 0 && year % 100 != 0)||(year % 400 == 0);   // the boolean formula determines if it is a leap year or not 
                         
                         if (leap == true){
                          System.out.println("The month has 29 days");
@@ -47,30 +48,27 @@ public class Month {
                         }
                     }
                 }
-                
+                // if year input was incorrect, the program returns the following error:
                 else {
                     System.out.println("You did not enter an integer");
                 }
-                
             }
-            
-            else if (month % 2 == 0 && month != 2 && month != 6) {
+            // else if determines if the month has 31 days 
+            else if (month ==1 || month ==3 || month ==5 || month ==7 || month ==8 || month ==10 || month ==12) {
+                System.out.println("The month has 31 days");
+            }
+            // else determines that the month has 30 days 
+            else {
                 System.out.println("The month has 30 days");
             }
             
-            else {
-                System.out.println("The month has 31 days");
-            }
-            
-           
         }
-            
+        
+        // if the original input was not an integer, the program returns an error message and terminates     
         else {
             System.out.println("You did not enter an integer");
         }
-        
-        
-        
+
     System.out.println(" "); // prints out an empty line for visual appealling
     }
 }
