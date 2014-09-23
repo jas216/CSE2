f1072172eabb80ccfa2da3676d739d998c741fa5
Jose Sierra <jas216@lehigh.edu>
2014-09-23 23:14:08 +0000

    IncomeTax

--- hw04/CourseNumber.java
+++ hw04/CourseNumber.java
@@ -7,3 +7,31 @@ semester accordign to the lehigh university semester numbering system */
 
 import java.util.Scanner;   // imports a user input device
 
+// define class
+public class CourseNumber{
+    // define main method
+    public static void main(String[] args){
+        System.out.println(" "); // prints out an empty line for visual appealling
+        // declare variables
+        Scanner myScanner;
+        myScanner = new.Scanner(System.in);
+        int courseNumber, spring, summer 1, summer 2, fall;
+        
+        
+        System.out.println("Enter a six digit semester course number: ");
+        // check for integer input
+        if (myScanner.hasNextInt()){
+            courseNumber = myScanner.nextInt();
+            
+            
+        }
+        
+        else{
+            
+        }
+        
+        
+        
+    }
+}
+
